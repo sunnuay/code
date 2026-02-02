4a80aca8da6fd06727a9ed8ece91a3cbcdcb700a
sunnuay <sunnuay@outlook.com>
2026-02-02 22:48:03 +0800

    No commit message

--- sort/main.go
+++ sort/main.go
@@ -1,15 +1,9 @@
-//go:build ignore
-
 package main
 
-import (
-	"fmt"
-
-	"code/sort"
-)
+import "fmt"
 
 func main() {
 	arr := []int{5, 12, 15, 11, 3, 8, 13, 9, 4, 7, 10, 2, 14, 1, 6}
-	sort.Ints(arr)
+	Ints(arr)
 	fmt.Println(arr)
 }
