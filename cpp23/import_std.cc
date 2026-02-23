101778f0d74cded6f3c8bf057b29df0efdefec01
sunnuay <sunnuay@outlook.com>
2026-02-23 23:02:47 +0800

    No commit message

---
+++ cpp23/import_std.cc
@@ -0,0 +1,6 @@
+// g++ -std=c++23 -fmodules -fsearch-include-path -c bits/std.cc
+// g++ -std=c++23 -fmodules import_std.cc
+
+import std;
+
+int main() { std::println("hello, world"); }
---
+++ cpp26/reflection.cc
@@ -0,0 +1,22 @@
+// g++ -std=c++26 -freflection reflection.cc
+
+#include <meta>
+#include <print>
+
+using namespace std::meta;
+
+template <typename T> void print(T const &t) {
+  template for (constexpr auto e :
+                define_static_array(nonstatic_data_members_of(
+                    ^^T, access_context::current()))) {
+    std::println(".{}={}", identifier_of(e), t.[:e:]);
+  }
+}
+
+int main() {
+  struct S {
+    int a, b;
+  };
+  S s{97, 98};
+  print(s);
+}
