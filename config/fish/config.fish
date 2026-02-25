04f376069977b51049bf2b4636bb13763ae20b54
sunnuay <sunnuay@outlook.com>
2026-02-25 23:16:26 +0800

    No commit message

---
+++ config/fish/config.fish
@@ -0,0 +1,18 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+functions -e fish_greeting
+
+function proxy_on
+    set -gx http_proxy http://127.0.0.1:7893
+    set -gx https_proxy http://127.0.0.1:7893
+    set -gx all_proxy socks5://127.0.0.1:7893
+end
+
+function proxy_off
+    set -e http_proxy
+    set -e https_proxy
+    set -e all_proxy
+end
+
+set -gx GOPATH $HOME/.go
+set -gx GOOGLE_CLOUD_PROJECT $USER
