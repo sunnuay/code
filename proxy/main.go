3846a2f4625e0314c0aa73e35077559f1a961131
sunnuay <sunnuay@outlook.com>
2026-03-05 22:35:16 +0800

    No commit message

--- proxy/main.go
+++ proxy/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
+type Config struct {
+	Listen int `yaml:"listen"`
+}
+
 func main() {
+	data, err := os.ReadFile("proxy/config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		log.Fatal(err)
+	}
 	targetURL := "http://www.example.com"
 	target, err := url.Parse(targetURL)
 	if err != nil {
@@ -18,7 +34,8 @@ func main() {
 			pr.SetURL(target)
 		},
 	}
-	if err := http.ListenAndServe(":8080", proxy); err != nil {
+	addr := ":" + fmt.Sprint(cfg.Listen)
+	if err := http.ListenAndServe(addr, proxy); err != nil {
 		log.Fatal(err)
 	}
 }
