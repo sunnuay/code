773356b7230cbef499bcd85a16ad88d2e0043ce4
sunnuay <sunnuay@outlook.com>
2026-04-05 16:23:39 +0800

    No commit message

--- src/cpp26/main.cpp
+++ src/cpp26/main.cpp
@@ -1,13 +1,23 @@
 #include <print>
 
-template <typename... Args> void print_first_and_last(Args... args) {
-  constexpr auto last_idx = sizeof...(Args) - 1;
-  std::println("First: {}, Last: {}", args...[0], args...[last_idx]);
+#if __has_include(<meta>)
+#include <meta>
+using namespace std::meta;
+template <typename T> void print_all(T const &t) {
+  template for (constexpr auto e : define_static_array(nonstatic_data_members_of(^^T, access_context::current()))) {
+    std::println(".{}={}", identifier_of(e), t.[:e:]);
+  }
 }
+#else
+template <typename T> void print_all(T const &t) {
+  auto const &[... e] = t;
+  (std::println("{}", e), ...);
+}
+#endif
 
 int main() {
-  auto [_, _, z] = std::make_tuple(1, 2, 3);
-  std::println("z: {}", z);
-
-  print_first_and_last("a", "b", "c");
+  struct S {
+    int a, b;
+  } s{97, 98};
+  print_all(s);
 }
