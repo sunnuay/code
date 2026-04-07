8cf83affadca0fb125d03b27bbf709cb421a0f64
sunnuay <sunnuay@outlook.com>
2026-04-07 11:17:43 +0800

    No commit message

--- server/main.go
+++ server/main.go
@@ -16,68 +16,44 @@ type grpcServer struct {
 	api.UnimplementedCoreServer
 }
 
-func (s *grpcServer) Handle(ctx context.Context, req *api.Request) (*api.Response, error) {
-	return &api.Response{ResponseText: "Hello " + req.RequestText + " from gRPC!"}, nil
+func (s *grpcServer) Handle(_ context.Context, req *api.Request) (*api.Response, error) {
+	return &api.Response{ResponseText: req.RequestText + "..."}, nil
 }
 
 func startGRPCServer() {
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":9998")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
 	api.RegisterCoreServer(s, &grpcServer{})
-
-	log.Println("gRPC Server running on :50051")
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve gRPC: %v", err)
+		log.Fatalf("Failed to run gRPC: %v", err)
 	}
 }
 
 func startGinServer() {
-	conn, err := grpc.NewClient(
-		"localhost:50051",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
+	conn, err := grpc.NewClient(":9998", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect to gRPC server: %v", err)
+		log.Fatalf("Failed to connect to gRPC: %v", err)
 	}
-	defer func() {
-		if err := conn.Close(); err != nil {
-			log.Printf("Failed to close gRPC connection: %v", err)
-		}
-	}()
 
 	grpcClient := api.NewCoreClient(conn)
-
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	if err := r.SetTrustedProxies(nil); err != nil {
-		log.Fatalf("Failed to set trusted proxies: %v", err)
-	}
-
-	r.GET("/hello/:name", func(c *gin.Context) {
-		name := c.Param("name")
-
-		req := &api.Request{RequestText: name}
-
-		resp, err := grpcClient.Handle(context.Background(), req)
+	r.GET("/:name", func(c *gin.Context) {
+		resp, err := grpcClient.Handle(c.Request.Context(), &api.Request{RequestText: c.Param("name")})
 		if err != nil {
 			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
-
-		c.JSON(200, gin.H{
-			"code": 200,
-			"data": resp.ResponseText,
-		})
+		c.JSON(200, gin.H{"code": 200, "data": resp.ResponseText})
 	})
 
-	log.Println("Gin HTTP Server running on :8080")
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run Gin server: %v", err)
+	if err := r.Run(":10002"); err != nil {
+		log.Fatalf("Failed to run Gin: %v", err)
 	}
 }
 
